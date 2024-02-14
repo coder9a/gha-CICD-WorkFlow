bcf4fe0c54c759ab9ad512c297ed1272e935a56a
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-02-14 19:48:48 +0530

    Feature/app v1 (#2)
    * checking run-test and build workflow
    
    * checking run-test and build workflow pr check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
+        <h2>New PR</h2>
       </div>
       <Link href="/about">About</Link>
       <br />
