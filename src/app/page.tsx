b001e1076e927f4514c7d41c2e9e2ecbdb3223ff
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-02-20 16:42:10 +0000

    npm fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,19 @@
-import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import flagsmith from "@/utils/flagsmith";
 
 export const revalidate = 0;
 
-export default function Home() {
-  const [searchEnabled, setSearchEnabled] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-
-  useEffect(() => {
-    async function fetchData() {
-      const flags = await flagsmith.getEnvironmentFlags();
-      setSearchEnabled(flags.isFeatureEnabled("search"));
-    }
-    fetchData();
-  }, []);
-
-  const handleSearchChange = (event) => {
-    setSearchValue(event.target.value);
-    // You can add further functionality here if needed
-  };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    // You can add functionality here to handle search submission
-  };
-
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>Test Changeset2</p>
+        <p>Test Changeset3</p>
       </div>
       <Link href="/about">About</Link>
       <br />
-      {searchEnabled && (
-        <form onSubmit={handleSearchSubmit}>
-          <input
-            type="text"
-            placeholder="Search"
-            value={searchValue}
-            onChange={handleSearchChange}
-          />
-          <button type="submit">Search</button>
-        </form>
-      )}
+      {flags.isFeatureEnabled("search") && <input placeholder="Search" />}
     </main>
   );
 }
